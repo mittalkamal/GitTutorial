db524db7a39d2b8286ef0902200bc2f1b31e8623
kamal kant agarwal <kamalmittal92@gmail.com>
2017-05-30 18:52:20 +0530

    Update ViewController.swift

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -12,9 +12,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("FromGitHub")
-        
-        print("updateFromXcode")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
