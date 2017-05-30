619822c065205ebf4a664b97f96ba71359e9c383
kamal <kamalmittal92@gmail.com>
2017-05-30 22:16:07 +0530

    update vc

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
+        print("Bye")
     }
 
     override func didReceiveMemoryWarning() {
